e0373d57b19f083470f5bb107297272e29568032
Aman Bhargava <amanbhargava2001@gmail.com>
2021-05-30 12:54:29 +0530

    Fonts not Loading

---
+++ themes/amnbh/assets/css/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    theme: {
+        nightwind: {
+            transitionDuration: "500ms", // default '300ms'
+        },
+        extend: {
+            colors: {
+                'orange-400': '#F85E00',
+                'orange-300': '#E86E23',
+                'orange-600': '#AB511A',
+                'orange-200': '#FC914E',
+                'biga': '#455434'
+            },
+            borderWidth: {
+                '3': '3px'
+            },
+            height: {
+                'h-screen-half': '50vh',
+                "screen/3": "calc(100vh / 3)",
+                "screen/4": "calc(100vh / 4)",
+                "screen/5": "calc(100vh / 5)",
+            },
+            fontFamily: {
+                'causten': ['Causten'],
+            }
+        }
+
+    },
+    variants: {},
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+    darkMode: "class",
+    // ...
+    plugins: [require("nightwind")]
+}
