5d470a2fd21d502c4662979f3dc896567c10723b
Aman Bhargava <amanbhargava2001@gmail.com>
2021-09-20 23:12:12 +0530

    Added Timelines and Counts

--- static/manifesto/js/counter.js
+++ static/manifesto/js/counter.js
@@ -67,3 +67,21 @@ function result() {
     var nodes = document.getElementById(n).getElementsByTagName("button");
     for(var i=0; i<nodes.length; i++) {nodes[i].setAttribute("disabled","true");}
   }
+
+  var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
